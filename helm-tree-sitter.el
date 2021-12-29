fc66e90970ace26b9ede2424940c769ff3c66473
Giedrius <giedriusj1@gmail.com>
2021-12-29 18:01:45 +0000

    add LICENSE

--- helm-tree-sitter.el
+++ helm-tree-sitter.el
@@ -8,7 +8,7 @@
 
 ;; Package-Requires: ((emacs "25.1") (helm "3.6.2") (tree-sitter "0.16.1"))
 
-;; This program is free software; you can redistribute it and/or modify
+;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
@@ -19,7 +19,7 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; Simple helm interface to tree-sitter.
